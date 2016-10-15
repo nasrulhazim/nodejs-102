5ac04a1bb6ed6b65312ff39a81b2ce9f44dad4dd
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-10-15 11:19:44 +0800

    working with header information

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var express = require('express');
+var http = require('http');
 var app = express();
+
+app.use(function(req, res, next){
+	// remove x-powered-by
+	res.removeHeader('X-Powered-By');
+	// custom header
+	res.setHeader('custom', 123);
+	console.log('Method:', req.method, '; URL: ', req.url, '; Time: ',  new Date());
+	next();
+});
 
 app.get('/', function(req, res){
 	res.send('hello');
@@ -16,7 +26,8 @@ app.get('/users/details/:id?/', function(req, res){
 	if(id == undefined)
 		res.send('user id not provided');
 
-	res.send('user id: ' + id);
+	// res.send('user id: ' + id);
+	res.send(req.params);
 });
 
 app.listen(3000, function(){
