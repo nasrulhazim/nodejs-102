fb32f309a3636f86e0eee78501100159cda1f1b9
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-10-15 10:56:24 +0800

    route & parameter

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ app.get('/', function(req, res){
 	res.send('hello');
 });
 
-app.get('/user', function(req, res){
+app.get('/users', function(req, res){
 	res.send('hello user');
+});
+
+// use ? after parameter to add as optional parameter
+app.get('/users/details/:id?/', function(req, res){
+	var id = req.params.id;
+
+	if(id == undefined)
+		res.send('user id not provided');
+
+	res.send('user id: ' + id);
 });
 
 app.listen(3000, function(){
